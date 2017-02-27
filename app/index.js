883378409df8ef6db4cc27860ea2dfc2b4e74987
WilliamSavage <williamsavage@hotmail.com>
2017-02-27 15:30:24 +0000

    config sass loaders css loader and style loader

--- app/index.js
+++ app/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom'
 import { Route, Router, browserHistory } from 'react-router'
 import AgeCheck from './containers/Age-check'
 import Home from './components/Home'
+import ContentForm from './components/ContentForm'
 
 ReactDOM.render(
   <Router history={browserHistory}>
     <Route path='/' component={Home} />
     <Route path='/age-check' component={AgeCheck} />
+    <Route path='/form' component={ContentForm} />
   </Router>,
   document.getElementById('root')
 )
