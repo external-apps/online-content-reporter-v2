fe899bbfff5f2a966405b4fda2eb8c3c8f811495
WilliamSavage <williamsavage@hotmail.com>
2017-02-27 13:55:21 +0000

    creates basic react components

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
-const App = () => (
+const Home = () => (
   <div>
-      <h1>Childline</h1>
+      <h1>Childline HOME</h1>
   </div>
 )
 
-export default App
+export default Home
--- app/containers/Age-check.js
+++ app/containers/Age-check.js
@@ -3,7 +3,7 @@ import React from 'react'
 class AgeCheck extends React.Component {
   render() {
     return (
-      <h1>Age check</h1>
+      <h1>Age check Me</h1>
     )
   }
 }
