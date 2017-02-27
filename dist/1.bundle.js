4ecdd971bb447d46c0cdabf2c1d8785a398078c9
WilliamSavage <williamsavage@hotmail.com>
2017-02-27 14:00:04 +0000

    creates entry reaction componenet

---
+++ dist/1.bundle.js
@@ -0,0 +1,28 @@
+webpackJsonp([1],{
+
+/***/ 234:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var App = function App() {
+  return React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "h1",
+      null,
+      "Childline App Report"
+    )
+  );
+};
+
+exports.default = App;
+
+/***/ })
+
+});
