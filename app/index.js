f61c2cd0f8d0dcf44a7e6b1c107743383973bc2c
WilliamSavage <willsavage@hotmail.com>
2017-03-05 20:09:56 +0200

    #45 creates store in index.js and wrapps in provider component

--- app/index.js
+++ app/index.js
@@ -6,14 +6,25 @@ import Home from './components/Home'
 import ContentForm from './components/ContentForm'
 import UrlForm from './components/UrlForm'
 import OverAge from './components/OverAge'
+import { Provider } from 'react-redux'
+import { createStore, combineReducers } from 'redux'
+import qr from './reducers/qr'
+
+const reducers = combineReducers({
+  qr: qr
+})
+
+const store = createStore(reducers)
 
 ReactDOM.render(
-  <Router history={browserHistory}>
-    <Route path='/' component={Home} />
-    <Route path='/age-check' component={AgeCheck} />
-    <Route path='/form' component={ContentForm} />
-    <Route path='/form2' component={UrlForm} />
-    <Route path='/over-age' component={OverAge} />
-  </Router>,
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path='/' component={Home} />
+      <Route path='/age-check' component={AgeCheck} />
+      <Route path='/form' component={ContentForm} />
+      <Route path='/form2' component={UrlForm} />
+      <Route path='/over-age' component={OverAge} />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 )
