bffc608faa38651c0ebcacf08b00978573dfaa5f
WilliamSavage <willsavage@hotmail.com>
2017-03-06 09:54:55 +0200

    removes console log from ar reducer

--- app/reducers/qr.js
+++ app/reducers/qr.js
@@ -1,8 +1,6 @@
 const qr = (state = {}, action) => {
-  console.log('action dispatch for', action.type)
   switch (action.type) {
     case 'ADD_QR_CODE':
-    console.log("adding SVG to state")
       return {
         qrSvg: action.qrSvg
       }
