78986c0c4c6ded67ebf5d6d7eedd10b657c8cf75
WilliamSavage <willsavage@hotmail.com>
2017-03-06 14:13:49 +0200

    import action types into actions and use spread operattor

--- app/actions/qr.js
+++ app/actions/qr.js
@@ -1,6 +1,28 @@
-export const addQrAction = (qrSvg) => {
+import * as types from '../../constants/action-types.js'
+
+export const addQr = (qrSvg) => {
   return {
-    type: 'ADD_QR_CODE',
+    type: types.ADD_QR_CODE,
     qrSvg
+  }
+}
+
+export const targetSelf = () => {
+  return {
+    type: types.TARGET_SELF,
+    target: '_self'
+  }
+}
+
+export const setHref = (href) => {
+  return {
+    type: types.SET_HREF,
+    href
+  }
+}
+
+export const toggleMobile = () => {
+  return {
+    type: types.TOGGLE_MOBILE
   }
 }
