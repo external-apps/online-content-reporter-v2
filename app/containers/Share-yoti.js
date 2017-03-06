0e8866e6d3db29e70d99d402b4cb3c72197a229e
WilliamSavage <willsavage@hotmail.com>
2017-03-06 09:52:40 +0200

    #45 completes yoti flow and redirects to relavant react route

--- app/containers/Share-yoti.js
+++ app/containers/Share-yoti.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import '../scss/style.scss'
-// import axios from 'axios'
 import { addQrAction } from '../actions/qr'
-console.log('tester', addQrAction('test'))
-import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-
 import QrCode from '../components/QrCode'
+import { browserHistory } from 'react-router'
 
 var t = {
   config: {
@@ -15,38 +12,6 @@ var t = {
   },
   appId: '3392788e-e529-4309-8ed7-54d7ac554055',
   scenId: '5be10ae7-af29-40b0-8d33-a0fb90cb0e88'
-}
-
-const listenForToken = (proto, url) => {
-  var host = 'wss://api.yoti.com/api/v1/connect-sessions/' + proto
-  var socket = new WebSocket(host)
-  socket.onopen = () => {
-    socket.send(JSON.stringify({subscription: proto}))
-  }
-  socket.onmessage = (msg) => {
-    var data = JSON.parse(msg.data)
-    switch (data.status) {
-      case 'COMPLETED' : {
-        window.location = 'thankyou?token=' + data.token
-      }
-    }
-  }
-}
-
-const mobileSetup = () => {
-  var url = 'https://www.yoti.com/qr/' + t.scenId
-  var xhr = new XMLHttpRequest
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      var responseObj = JSON.parse(xhr.responseText)
-      // here we assign the url to the link!
-      this.setState({href: responseObj.qrCodeUrl + '?callback=' + responseObj.callbackUrl + '&id=' + responseObj.application.id + '&mobile=' + true})
-    }
-  }
-  xhr.open('GET', url, true)
-  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
-  xhr.setRequestHeader('content-type', 'application/json')
-  xhr.send(null)
 }
 
 class ShareYoti extends React.Component {
@@ -58,12 +23,56 @@ class ShareYoti extends React.Component {
       target: '_blank',
       qr: false
     }
-    this.handleClick = this.startVerification.bind(this)
+
+    this.startVerification = this.startVerification.bind(this)
     this.getQr = this.getQr.bind(this)
+    this.listenForToken = this.listenForToken.bind(this)
+    this.yotiRedirect = this.yotiRedirect.bind(this)
+  }
+
+  listenForToken (proto, url) {
+    var host = 'wss://api.yoti.com/api/v1/connect-sessions/' + proto
+    var socket = new WebSocket(host)
+    socket.onopen = () => {
+      socket.send(JSON.stringify({subscription: proto}))
+    }
+    socket.onmessage = (msg) => {
+      var data = JSON.parse(msg.data)
+      switch (data.status) {
+        case 'COMPLETED' : {
+          this.yotiRedirect(data.token)
+        }
+      }
+    }
+  }
+
+  yotiRedirect (token) {
+    var xhr = new XMLHttpRequest()
+    xhr.addEventListener('load', (e) => {
+      var responseObj = JSON.parse(e.target.responseText)
+      responseObj.isUnder18 ? browserHistory.push('/form') : browserHistory.push('/over-age')
+    })
+    xhr.open('GET', `/thankyou?token=${token}`)
+    xhr.send()
+  }
+
+  mobileSetup () {
+    var url = 'https://www.yoti.com/qr/' + t.scenId
+    var xhr = new XMLHttpRequest
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        var responseObj = JSON.parse(xhr.responseText)
+        this.setState({href: responseObj.qrCodeUrl + '?callback=' + responseObj.callbackUrl + '&id=' + responseObj.application.id + '&mobile=' + true})
+      }
+    }
+    xhr.open('GET', url, true)
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
+    xhr.setRequestHeader('content-type', 'application/json')
+    xhr.send(null)
   }
 
   componentDidMount () {
-    /webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini|Android/i.test(navigator.userAgent) && /Mobile/i.test(navigator.userAgent) ? (this.setState({isMobile: true, target: '_self'}), mobileSetup()) : (this.setState({href: '#'}))
+    /webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini|Android/i.test(navigator.userAgent) && /Mobile/i.test(navigator.userAgent) ? (this.setState({isMobile: true, target: '_self'}), this.mobileSetup()) : console.log('Running on desktop')
   }
 
   startVerification () {
@@ -75,21 +84,20 @@ class ShareYoti extends React.Component {
     xhr.addEventListener('load', (e) => {
       var responseObj = JSON.parse(e.target.responseText)
       this.props.addQrAction(responseObj.svg)
-      listenForToken(responseObj.proto, responseObj.url)
+      this.listenForToken(responseObj.proto, responseObj.url)
     })
     xhr.open('GET', '/qr')
     xhr.send()
   }
 
   navigateToYoti () {
-    // window.location = t.config.service + appI
     t.serviceRedirectTimeout && clearTimeout(t.serviceRedirectTimeout)
-    var n = Date.now()
-    var r = 5e3
-    t.serviceRedirectTimeout = setTimeout(function () {
-      var e = Date.now()
-      r + 1e3 > e - (n + r) && (window.location = t.config.service + t.appId)
-    }, r)
+    var current = Date.now()
+    var hangTime = 5e3
+    t.serviceRedirectTimeout = setTimeout(() => {
+      var timeAfter = Date.now()
+      hangTime + 1e3 > timeAfter - (current + hangTime) && (window.location = t.config.service + t.appId)
+    }, hangTime)
   }
 
   render () {
@@ -102,9 +110,8 @@ class ShareYoti extends React.Component {
         <a
           href={this.state.href}
           className='btn yoti-connect learn-more btn-primary'
-          data-target='embed'
-          data-yoti-type='inline'
-          data-size='medium'>
+          target='self'
+          >
           Share your age using Yoti
         </a>
       </span>
