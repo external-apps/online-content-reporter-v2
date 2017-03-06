c2c2529d081e1d248b99e290ac4ce9c5c65e270f
WilliamSavage <willsavage@hotmail.com>
2017-03-06 14:15:35 +0200

    refactors reducers to use action types

--- app/reducers/qr.js
+++ app/reducers/qr.js
@@ -1,11 +1,40 @@
-const qr = (state = {}, action) => {
+import {
+  ADD_QR_CODE,
+  TARGET_SELF,
+  SET_HREF,
+  TOGGLE_MOBILE
+} from '../../constants/action-types'
+
+const initialState = {
+  isMobile: false,
+  href: `#`,
+  target: undefined
+}
+
+const qr = (state = initialState, action) => {
   switch (action.type) {
-    case 'ADD_QR_CODE':
+    case ADD_QR_CODE:
       return {
+        ...state,
         qrSvg: action.qrSvg
       }
+    case TARGET_SELF:
+      return {
+        ...state,
+        target: action.target
+      }
+    case SET_HREF:
+      return {
+        ...state,
+        href: action.href
+      }
+    case TOGGLE_MOBILE:
+      return {
+        ...state,
+        isMobile: !state.isMobile
+      }
     default:
-      return {}
+      return state
   }
 }
 
