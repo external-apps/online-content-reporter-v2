b06bff62a9931997f19d0dfd6e5b3dadc88cac23
denesnori <denes.nori@yahoo.com>
2017-03-06 19:47:55 +0000

    #51 remove modal.js, it is not needed enymore fix modal size

--- app/components/UrlForm.js
+++ app/components/UrlForm.js
@@ -8,14 +8,30 @@ var Modal = require('react-modal')
 injectTapEventPlugin()
 
 const customStyles = {
-  content : {
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
-  }
+
+    overlay : {
+      position          : 'fixed',
+      top               : 0,
+      left              : 0,
+      right             : 0,
+      bottom            : 0,
+      backgroundColor   : 'rgba(255, 255, 255, 0.75)'
+    },
+    content : {
+      position                   : 'absolute',
+      top                        : '5rem',
+      left                       : '10rem',
+      right                      : '10rem',
+      bottom                     : '5rem',
+      border                     : '1px solid #ccc',
+      background                 : '#fff',
+      overflow                   : 'hidden',
+      WebkitOverflowScrolling    : 'touch',
+      borderRadius               : '0.25rem',
+      outline                    : 'none',
+      padding                    : '1rem'
+
+    }
 };
 
 
