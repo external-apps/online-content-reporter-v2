6d1eced3267f3843107d3ef4482d6bd7f491c2f1
WilliamSavage <willsavage@hotmail.com>
2017-03-07 17:37:39 +0200

    #56 renames qr actions

---
+++ app/actions/yoti.js
@@ -0,0 +1,21 @@
+import * as types from '../../constants/action-types.js'
+
+export const addQr = (qrSvg) => {
+  return {
+    type: types.ADD_QR_CODE,
+    qrSvg,
+    haveQr: true
+  }
+}
+
+export const setUpForMobile = (href) => {
+  return {
+    type: types.SET_UP_FOR_MOBILE,
+    href,
+    target: '_self'
+  }
+}
+
+export const getQr = () => {
+  console.log('here we will do the yoti desktop flow?')
+}
