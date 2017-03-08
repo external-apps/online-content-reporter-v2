32ee89a336cc080841683a14d1f40d4064315ad1
denesnori <denes.nori@yahoo.com>
2017-03-08 17:45:17 +0000

    fixes

--- app/components/PageTitle.js
+++ app/components/PageTitle.js
@@ -2,7 +2,7 @@ import React from 'react'
 import '../scss/style.scss'
 
 const PageTitle = (props) => (
-  <div className='title-container'>
+  <div className={`title-container ${props.backgroundColor}`}>
     <h1 className='page-title-heading'>{props.heading}</h1>
     <p className='page-title-subheading'>
       {props.subheading}
