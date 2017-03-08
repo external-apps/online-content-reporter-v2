03370fcfcd452b311ad08b875cff4df57f707380
denesnori <denes.nori@yahoo.com>
2017-03-08 21:18:20 +0000

    #75 new folder structure

---
+++ test/app/components/agecheck.tests.js
@@ -0,0 +1,10 @@
+/*describe('onClik tests', () => {
+  it('simulates click events', () => {
+     const onClick = sinon.spy();
+     const wrapper = mount(
+       <Foo onClick={onButtonClick} />
+     )
+     wrapper.find('button').simulate('click');
+     expect(onClick).to.have.property('callCount', 1);
+   })
+})*/
