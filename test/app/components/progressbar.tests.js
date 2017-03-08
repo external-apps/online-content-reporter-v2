8b4ab27ba085319647bdadb88f66e8a84041a4c1
denesnori <denes.nori@yahoo.com>
2017-03-08 20:42:48 +0000

    75 add pagetitle and progressbar tests

--- test/app/components/progressbar.tests.js
+++ test/app/components/progressbar.tests.js
@@ -1,13 +1,10 @@
 import { expect } from 'chai'
 import React from 'react'
-import { shallow, mount, render } from 'enzyme'
+import { shallow } from 'enzyme'
 import ProgressBar from '../../../app/components/ProgressBar'
-import sinon from 'sinon'
-import { Link } from 'react-router'
-
 
 describe('<ProgressBar />', () => {
-  it('renders an home', () => {
+  it('renders an element with progress-bar class', () => {
     const wrapper = shallow(<ProgressBar />)
     expect(wrapper.find('.progress-bar')).to.have.length(1)
   })
