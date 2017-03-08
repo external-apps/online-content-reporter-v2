da6026635f8d96792df17f1263f446dced7d6298
WilliamSavage <willsavage@hotmail.com>
2017-03-08 17:16:13 +0200

    change system for naming Pagetitle classes

--- app/components/PageTitle.js
+++ app/components/PageTitle.js
@@ -3,8 +3,8 @@ import '../scss/style.scss'
 
 const PageTitle = (props) => (
   <div className={`title-container ${props.backgroundColor}`}>
-    <h1 className='title-heading'>{props.heading}</h1>
-    <p className='title-subheading'>
+    <h1 className='page-title-heading'>{props.heading}</h1>
+    <p className='page-title-subheading'>
       {props.subheading}
     </p>
   </div>
