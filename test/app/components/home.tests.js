c697791616bdfce768006feb9067ebe5f7f73680
denesnori <denes.nori@yahoo.com>
2017-03-09 13:01:30 +0000

    update test descriptions for Home tests #75

--- test/app/components/home.tests.js
+++ test/app/components/home.tests.js
@@ -8,16 +8,16 @@ import Form from '../../../app/containers/Forms'
 import RaisedButton from 'material-ui/RaisedButton'
 import PageTitle from '../../../app/components/PageTitle'
 
-describe('<Home />', () => {
+describe('<Home /> should render an element with className home-header', () => {
   it('renders an home', () => {
     const wrapper = shallow(<Home />)
     expect(wrapper.find('.home-header')).to.have.length(1)
   })
-   it('renders one <RaisedButton/> element', () => {
+   it('<Home /> should render a <RaisedButton/> element', () => {
     const wrapper = shallow(<Home />)
     expect(wrapper.find(RaisedButton)).to.have.length(1)
   })
-  it('renders one <Link /> element', () => {
+  it('<Home /> should render a <Link /> element', () => {
    const wrapper = shallow(<Home />)
    expect(wrapper.find(Link)).to.have.length(2) // update the test when we remove the other link
  })
@@ -28,27 +28,27 @@ describe('<Home /> children node tests', () => {
     const wrapper = shallow(<Home />)
     expect(wrapper.find('.home-header-info')).to.have.length(1)
   })
-  it('renders an home', () => {
+  it('<Home /> should render an element with className home-header-phone', () => {
     const wrapper = shallow(<Home />)
     expect(wrapper.find('.home-header-phone')).to.have.length(1)
   })
-  it('renders an home', () => {
+  it('<Home /> should render an element with className home-title', () => {
     const wrapper = shallow(<Home />)
     expect(wrapper.find('.home-title')).to.have.length(1)
   })
-  it('renders an home', () => {
+  it('<Home /> should render an element with className home-info', () => {
     const wrapper = shallow(<Home />)
     expect(wrapper.find('.home-info')).to.have.length(1)
   })
-  it('renders an home', () => {
+  it('<Home /> should render an element with className section-title', () => {
     const wrapper = shallow(<Home />)
     expect(wrapper.find('.section-title')).to.have.length(1)
   })
-  it('renders an home', () => {
+  it('<Home /> should render an element with className home-logos', () => {
     const wrapper = shallow(<Home />)
     expect(wrapper.find('.home-logos')).to.have.length(1)
   })
-  it('renders an home', () => {
+  it('<Home /> should render 2 img elements', () => {
     const wrapper = shallow(<Home />)
     expect(wrapper.find('img')).to.have.length(2)
   })
