d76f3b73958ce9b3fe2d7802b7bb486306d48f45
denesnori <denes.nori@yahoo.com>
2017-03-09 11:20:21 +0000

    #75 more children node test on home

--- test/app/components/home.tests.js
+++ test/app/components/home.tests.js
@@ -32,4 +32,16 @@ describe('<Home /> children node tests', () => {
     const wrapper = shallow(<Home />)
     expect(wrapper.find('.home-header-phone')).to.have.length(1)
   })
+  it('renders an home', () => {
+    const wrapper = shallow(<Home />)
+    expect(wrapper.find('.home-title')).to.have.length(1)
+  })
+  it('renders an home', () => {
+    const wrapper = shallow(<Home />)
+    expect(wrapper.find('.home-info')).to.have.length(1)
+  })
+  it('renders an home', () => {
+    const wrapper = shallow(<Home />)
+    expect(wrapper.find('.home-logos')).to.have.length(1)
+  })
 })
