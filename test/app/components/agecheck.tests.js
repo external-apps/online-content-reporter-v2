878c8b5495a173373773e3822d8233c9e4fa711f
denesnori <denes.nori@yahoo.com>
2017-03-09 12:58:14 +0000

    update test descriptions #75

--- test/app/components/agecheck.tests.js
+++ test/app/components/agecheck.tests.js
@@ -32,27 +32,27 @@ describe('renders <AgeCheck />', () => {
     const barProps = wrapper.find('ProgressBar').props()
     expect(barProps.percentage).to.equal('33')
   })
-  it('renders ', () => {
+  it('<AgeCheck /> should render an element with .age-info-1 class', () => {
     const wrapper = shallow(<AgeCheck />)
     expect(wrapper.find('.age-info-1')).to.have.length(1)
   })
-  it('renders ', () => {
+  it('.age-info-1 class should have a <p> children', () => {
     const wrapper = shallow(<AgeCheck />)
     expect(wrapper.find('.age-info-1').childAt(0).type()).to.equal('p')
   })
-  it('renders ', () => {
+  it('.age-info-1 class should have a <p> children, which has an <a> children', () => {
     const wrapper = shallow(<AgeCheck />)
     expect(wrapper.find('.age-info-1').childAt(0).find('a')).to.have.length(1)
   })
-  it('renders ', () => {
+  it('<AgeCheck /> should render an element with .age-info-2 class', () => {
     const wrapper = shallow(<AgeCheck />)
     expect(wrapper.find('.age-info-2')).to.have.length(1)
   })
-  it('renders ', () => {
+  it('<AgeCheck /> should render 2 <a> tags', () => {
     const wrapper = shallow(<AgeCheck />) /*i guess they should be Links justto be consisten!!*/
     expect(wrapper.find('a')).to.have.length(2)
   })
-  it('renders ', () => {
+  it('<AgeCheck /> should render 2 <i> tags', () => {
     const wrapper = shallow(<AgeCheck />)
     expect(wrapper.find('i')).to.have.length(2)
   })
