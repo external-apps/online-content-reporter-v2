27cff28508a696cc3042dabbfed1af2b0fb89b6f
denesnori <denes.nori@yahoo.com>
2017-03-09 10:46:51 +0000

    more frontend test #75

--- test/app/components/pagetitle.tests.js
+++ test/app/components/pagetitle.tests.js
@@ -2,11 +2,15 @@ import { expect } from 'chai'
 import React from 'react'
 import { shallow } from 'enzyme'
 import PageTitle from '../../../app/components/PageTitle'
+const props = {
+   subheading :'I am a subtitle'
+};
 
 describe('<PageTitle />', () => {
   it('renders a PageTitle compoent', () => {
     const wrapper = shallow(<PageTitle />)
     expect(wrapper.find('div')).to.have.length(1)
+    expect(wrapper.children().length).to.equal(2)
   })
   it('renders a PageTitle compoent', () => {
     const wrapper = shallow(<PageTitle />)
@@ -15,5 +19,8 @@ describe('<PageTitle />', () => {
   it('renders a PageTitle compoent', () => {
     const wrapper = shallow(<PageTitle />)
     expect(wrapper.find('.page-title-subheading')).to.have.length(1)
+    expect(wrapper.find('p')).to.have.length(1)
+    wrapper.setProps({...props})
+    expect(wrapper.find('p').text()).to.equal('I am a subtitle')
   })
 })
