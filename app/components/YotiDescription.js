403724d65b59591eb6a369029050b63dc09a1c8b
WilliamSavage <willsavage@hotmail.com>
2017-03-09 12:11:57 +0200

    adds classs to Qr code container in order to cente

--- app/components/YotiDescription.js
+++ app/components/YotiDescription.js
@@ -6,18 +6,18 @@ const YotiDescription = (props) => {
   if (!props.yoti.haveQr) {
     return (
       <div className='yoti-info'>
-        <div className='yoti-intro'>
+        <div className='yoti-info-heading'>
           <h3>YOTI - CONFIDENTIALLY VERIFY YOUR AGE</h3>
           <img src='./imgs/yoti-logo.svg' alt='yoti logo' />
         </div>
-        <p className='yoti-description'>
+        <p className='yoti-info-description'>
           Yoti empowers you to take back control over your identity. If you don't have a Yoti digital identity, please create one <a className='link' href='www.yoti.com/'>here</a> and use it to verify you are under 17.
         </p>
       </div>
     )
   }
   return (
-    <div className='yoti-info '>
+    <div className='yoti-info yoti-center'>
       <QrCode {...props} />
     </div>
   )
