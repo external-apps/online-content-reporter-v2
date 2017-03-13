b948418b26a977816ad31c7b346da1080744d38b
denesnori <denes.nori@yahoo.com>
2017-03-13 17:16:40 +0000

    more footer tests #75

--- test/app/components/footer.tests.js
+++ test/app/components/footer.tests.js
@@ -4,12 +4,32 @@ import { shallow } from 'enzyme'
 import Footer from '../../../app/components/Footer'
 
 describe('<Footer /> ', () => {
-  it('<Home /> should render an element with className footer', () => {
+  it('<Footer /> should render an element with className footer', () => {
     const wrapper = shallow(<Footer />)
     expect(wrapper.find('.footer')).to.have.length(1)
   })
-  it('<Home /> should render an element with className footer', () => {
+  it('<Footer /> should render an element with className footer', () => {
     const wrapper = shallow(<Footer />)
     expect(wrapper.find('.footer-info')).to.have.length(1)
+  })
+  it('<Footer /> should render an element with className footer-logos', () => {
+    const wrapper = shallow(<Footer />)
+    expect(wrapper.find('.footer-logos')).to.have.length(1)
+  })
+  it('<Footer /> should render an img tag', () => {
+    const wrapper = shallow(<Footer />)
+    expect(wrapper.find('img')).to.have.length(1)
+  })
+  it('<Footer /> should render an element with src prop', () => {
+    const wrapper = shallow(<Footer />)
+    expect(wrapper.find('img').props().src).to.equal('imgs/iwf-logo.png')
+  })
+  it('<Footer /> should render an element with an alt prop', () => {
+    const wrapper = shallow(<Footer />)
+    expect(wrapper.find('img').props().alt).to.equal('IWF logo')
+  })
+  it('<Footer /> should render 3 a tags', () => {
+    const wrapper = shallow(<Footer />)
+    expect(wrapper.find('a')).to.have.length(3)
   })
 })
