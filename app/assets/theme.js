7351a2125f6bde484b49e8ede248d1b84ae4e03c
WilliamSavage <willsavage@hotmail.com>
2017-03-13 16:08:39 +0200

    #102 changes the colour of button in theme

--- app/assets/theme.js
+++ app/assets/theme.js
@@ -10,17 +10,17 @@ const muiTheme = getMuiTheme({
     textTransform: 'normal',
     fontFamily: 'childline-bold, sans-serif',
     primary1Color: '#0c3d52',   /* backgroundColor */
-    primary2Color: '#632c7a',
-    primary3Color: '#632c7a',
-    accent1Color: '#632c7a',
-    accent2Color: '#632c7a',
-    accent3Color: '#632c7a',
+    primary2Color: '#0c3d52',
+    primary3Color: '#0c3d52',
+    accent1Color: '#0c3d52',
+    accent2Color: '#0c3d52',
+    accent3Color: '#0c3d52',
     textColor: '#0c3d52',       /* general text color e.g.: in list items */
     alternateTextColor: '#fff', /* button text color */
-    canvasColor: '#632c7a',
-    borderColor: '#632c7a',
-    disabledColor: '#632c7a',
-    pickerHeaderColor: '#632c7a'
+    canvasColor: '#0c3d52',
+    borderColor: '#0c3d52',
+    disabledColor: '#0c3d52',
+    pickerHeaderColor: '#0c3d52'
   }
 })
 
