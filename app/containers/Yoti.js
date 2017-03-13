1685c4433e530edeb7b8240867f01d19dce201a7
WilliamSavage <willsavage@hotmail.com>
2017-03-13 18:46:15 +0200

    remvoes spacing

--- app/containers/Yoti.js
+++ app/containers/Yoti.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import * as yotiActions from '../actions/yoti'
-
 import YotiShareButton from '../components/YotiShareButton'
 import YotiDescription from '../components/YotiDescription'
 import '../scss/style.scss'
