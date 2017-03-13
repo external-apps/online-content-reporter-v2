1d920322f258cc6bce9985920833354e8697cc2c
denesnori <denes.nori@yahoo.com>
2017-03-13 11:42:42 +0000

    #75 more tests

--- test/app/components/imagecriteriaform.tests.js
+++ test/app/components/imagecriteriaform.tests.js
@@ -33,6 +33,10 @@ describe('<ImageCriteriaForm />', () => {
     const wrapper = shallow(<ImageCriteriaForm />)
     expect(wrapper.find('.form-container')).to.have.length(1)
   })
+  it('renders a component with className other-options', () => {
+    const wrapper = shallow(<ImageCriteriaForm />)
+    expect(wrapper.find('.other-options')).to.have.length(1)
+  })
   it('renders a <List> component ', () => {
     const wrapper = shallow(<ImageCriteriaForm />)
     expect(wrapper.find('List')).to.have.length(1)
@@ -40,5 +44,20 @@ describe('<ImageCriteriaForm />', () => {
   it('renders 5 <ListItem> components ', () => {
     const wrapper = shallow(<ImageCriteriaForm />)
     expect(wrapper.find('ListItem')).to.have.length(5)
+  })
+})
+
+describe('<ImageCriteriaForm /> teest props of several elements', () => {
+  it('PageTile component should have a heading prop', () => {
+    const wrapper = shallow(<ImageCriteriaForm />)
+    expect(wrapper.find('PageTitle').props().heading).to.equal('DESCRIBE THE ONLINE CONTENT')
+  })
+  it('PageTile component should have a backgroundColor prop', () => {
+    const wrapper = shallow(<ImageCriteriaForm />)
+    expect(wrapper.find('PageTitle').props().backgroundColor).to.equal('purple')
+  })
+  it('ProgreessBar component should have a percentage prop', () => {
+    const wrapper = shallow(<ImageCriteriaForm />)
+    expect(wrapper.find('ProgressBar').props().percentage).to.equal('66')
   })
 })
