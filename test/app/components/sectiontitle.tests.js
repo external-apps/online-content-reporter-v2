df9d02941ff7f52d4230c4fda1bf05852254ffcb
denesnori <denes.nori@yahoo.com>
2017-03-17 11:42:05 +0000

    #140 #75 final section title tests

--- test/app/components/sectiontitle.tests.js
+++ test/app/components/sectiontitle.tests.js
@@ -3,6 +3,12 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import SectionTitle from '../../../app/components/SectionTitle'
 
+const props = {
+  heading: 'Heading',
+  subheading: 'Subheading',
+  span: 'I am bold :D'
+}
+
 describe('<SectionTitle /> ', () => {
   it('<Header /> should render a section-title-container', () => {
     const wrapper = shallow(<SectionTitle />)
@@ -23,5 +29,23 @@ describe('<SectionTitle /> ', () => {
   it('<Header /> should render a span with className bold', () => {
     const wrapper = shallow(<SectionTitle />)
     expect(wrapper.find('span').props().className).to.equal('bold')
+  })
+})
+
+describe('<SectionTitle /> test props ', () => {
+  it('<SectionTitle /> should have a heading prop with the text "Heading"', () => {
+    const wrapper = shallow(<SectionTitle />)
+    wrapper.setProps({...props})
+    expect(wrapper.find('.section-title-heading').text()).to.equal('Heading')
+  })
+  it('<SectionTitle /> can receive a subheading prop and render it', () => {
+    const wrapper = shallow(<SectionTitle />)
+    wrapper.setProps({...props})
+    expect(wrapper.find('.section-title-subheading').text()).to.equal('Subheading I am bold :D')
+  })
+  it('<SectionTitle /> can receive a span prop and render it', () => {
+    const wrapper = shallow(<SectionTitle />)
+    wrapper.setProps({...props})
+    expect(wrapper.find('span').text()).to.equal(' I am bold :D')
   })
 })
