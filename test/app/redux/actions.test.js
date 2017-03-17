259ea08053307b33128374c80b275938bbc16adc
denesnori <denes.nori@yahoo.com>
2017-03-17 17:24:40 +0000

    #143 more action tests

--- test/app/redux/actions.test.js
+++ test/app/redux/actions.test.js
@@ -5,13 +5,25 @@ import * as formActions from '../../../app/actions/forms'
 import * as yotiActions from '../../../app/actions/yoti'
 
 import {
+  // yoti actions
   ADD_QR_CODE,
   SET_UP_FOR_MOBILE,
+  //form actions
   CHANGE_MODAL,
   CHANGE_FORM,
+  TOGGLE_CRITERIA,
   SAVE_URL,
   SAVE_DESCRIPTION,
-  TOGGLE_CRITERIA
+  SAVE_EMAIL,
+  SHOW_URL_REQUIRED_MESSAGE, 
+  CHANGE_OPTION1, //todo
+  CHANGE_OPTION2, //todo
+  CHANGE_OPTION3, //todo
+  CHANGE_OPTION4, //todo
+  CHANGE_OPTION5, //todo
+  VALID_EMAIL_REQUIRED_MESSAGE, //todo
+  HIDE_VALID_EMAIL_REQUIRED_MESSAGE, //todo
+  SHOW_CRITERIA_REQUIRED_MESSAGE, //todo
 } from '../../../constants/action-types'
 
 describe('Test actions', () => {
@@ -38,6 +50,22 @@ describe('Test actions', () => {
     expect(
       formActions.saveUrl()
     ).to.have.property('type','SAVE_URL')
+  })
+  it('should dispatch an action to save email', () => { //
+    const expectedAction = {
+      type: SAVE_EMAIL
+    }
+    expect(
+      formActions.saveEmail()
+    ).to.have.property('type','SAVE_EMAIL')
+  })
+  it('should dispatch an action to show url required message', () => { //
+    const expectedAction = {
+      type: SHOW_URL_REQUIRED_MESSAGE
+    }
+    expect(
+      formActions.showUrlRequiredMessage()
+    ).to.have.property('type','SHOW_URL_REQUIRED_MESSAGE')
   })
   it('should dispatch an action to save description', () => { //
     const expectedAction = {
