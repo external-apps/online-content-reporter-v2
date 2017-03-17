5ed5726f9f635c324e6a4316cc93a7440afb2d82
denesnori <denes.nori@yahoo.com>
2017-03-17 11:51:05 +0000

    #75 #140 progressbar tests

--- app/components/ProgressBar.js
+++ app/components/ProgressBar.js
@@ -3,9 +3,10 @@ import '../scss/style.scss'
 
 const ProgressBar = (props) => {
   return (
-  <div className="progress-bar">
-    <h2>Step {props.form==='1' ? 1 :2} of 2 </h2>
-  </div>
-)}
+    <div className='progress-bar'>
+      <h2>Step { props.form === '1' ? 1 :2} of 2</h2>
+    </div>
+  )
+}
 
 export default ProgressBar
