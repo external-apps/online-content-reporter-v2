028b291d16615180cf2ab681ebe0059c229688ae
denesnori <denes.nori@yahoo.com>
2017-03-17 14:12:49 +0000

    #140 #75 intitial agecheck tests

--- test/app/components/agecheck.tests.js
+++ test/app/components/agecheck.tests.js
@@ -2,3 +2,22 @@ import { expect } from 'chai'
 import React from 'react'
 import { shallow, mount, render } from 'enzyme'
 import AgeCheck from '../../../app/components/AgeCheckPage'
+
+describe('<AgeCheck />', () => {
+  it('renders a Header component', () => {
+    const wrapper = shallow(<AgeCheck />)
+    expect(wrapper.find('Header')).to.have.length(1)
+  })
+  it('renders a SectionTitle component', () => {
+    const wrapper = shallow(<AgeCheck />)
+    expect(wrapper.find('SectionTitle')).to.have.length(1)
+  })
+  /*it('renders a Yoti component', () => {
+    const wrapper = shallow(<AgeCheck />)
+    expect(wrapper.find('Yoti')).to.have.length(1)
+  })*/
+  it('renders a Footer component', () => {
+    const wrapper = shallow(<AgeCheck />)
+    expect(wrapper.find('Footer')).to.have.length(1)
+  })
+})
