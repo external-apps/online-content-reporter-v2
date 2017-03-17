74c6b9f04775e0f53cd0cc7ded726c66e22c244b
denesnori <denes.nori@yahoo.com>
2017-03-17 20:07:00 +0000

    make a new component to make the code more testable

--- app/components/YotiDescription.js
+++ app/components/YotiDescription.js
@@ -1,21 +1,12 @@
 import React from 'react'
 import QrCode from './QrCode'
 import '../scss/style.scss'
+import YotiVerify from './YotiVerify'
 
 const YotiDescription = (props) => {
-  console.log('PROPS TO LOOK ATL', props)
   if (!props.yoti.showQr) {
     return (
-      <div className='yoti-description-container'>
-        <h3 className='yoti-description-title'>VERIFY YOUR ARE USING YOTI</h3>
-        <span className='yoti-description'>
-          <img src='./imgs/yoti-logo.svg' alt='yoti logo' />
-          <p>If you've already got a YOTI account, click the button below and scan our QR code using your YOTI.</p>
-        </span>
-        <p className='yoti-instructions'>
-          If not, it's easy to <a className='link' href='www.yoti.com/'>sign up</a> using the YOTI app or website. YOTI lets you safely share your details online - Just click below to create your account.
-        </p>
-      </div>
+      <YotiVerify />
     )
   }
   return (
