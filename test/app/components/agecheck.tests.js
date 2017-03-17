7aed7d0989a3b069fe020304f9d8041082b0dfaf
denesnori <denes.nori@yahoo.com>
2017-03-17 14:39:48 +0000

    #140 #75 final agecheck tests

--- test/app/components/agecheck.tests.js
+++ test/app/components/agecheck.tests.js
@@ -20,4 +20,31 @@ describe('<AgeCheck />', () => {
     const wrapper = shallow(<AgeCheck />)
     expect(wrapper.find('Footer')).to.have.length(1)
   })
+ it('renders an element with agecheck-body class', () => {
+    const wrapper = shallow(<AgeCheck />)
+    expect(wrapper.find('.agecheck-body')).to.have.length(1)
+  })
+  it('renders an element with age-info class', () => {
+    const wrapper = shallow(<AgeCheck />)
+    expect(wrapper.find('.age-info')).to.have.length(1)
+  })
+  it('renders an element with age-info class has a child with "What can I do if I am over 18?"text', () => {
+    const wrapper = shallow(<AgeCheck />)
+    expect(wrapper.find('.age-info').childAt(0).text()).to.have.equal('What can I do if I am over 18?')
+  })
+  it('renders an element with age-info class has a p tag child', () => {
+    const wrapper = shallow(<AgeCheck />)
+    expect(wrapper.find('.age-info').find('p')).to.have.length(1)
+  })
+})
+
+describe('<AgeCheck /> SectionTitle props', () => {
+  it('has a heading "CONFIRM YOUR AGE"', () => {
+    const wrapper = shallow(<AgeCheck />)
+    expect(wrapper.find('SectionTitle').props().heading).to.equal('CONFIRM YOUR AGE')
+  })
+  it('has a heading "To remove an image or video, we just need to know your age. We can only remove this content if you are under 18."', () => {
+    const wrapper = shallow(<AgeCheck />)
+    expect(wrapper.find('SectionTitle').props().subheading).to.equal("To remove an image or video, we just need to know your age. We can only remove this content if you're under 18.")
+  })
 })
