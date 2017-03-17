fa57e67112e0279416d92668fc61f9cd35be90df
denesnori <denes.nori@yahoo.com>
2017-03-17 16:51:38 +0000

    close modal on unmounting

--- test/app/components/yotidescription.tests.js
+++ test/app/components/yotidescription.tests.js
@@ -2,3 +2,12 @@ import { expect } from 'chai'
 import React from 'react'
 import { shallow, mount, render } from 'enzyme'
 import YotiDescription from '../../../app/components/YotiDescription'
+const props = {
+  yoti: {
+    showQr: false
+  }
+}
+
+describe('<YotiDescription />', () => {
+
+})
