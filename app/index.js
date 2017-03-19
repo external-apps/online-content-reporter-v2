f6c18b1eda1b8d2301432fee574d33e81b8a1c70
WilliamSavage <willsavage@hotmail.com>
2017-03-19 10:47:51 +0200

    adds NotFound componenet

--- app/index.js
+++ app/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Route, Router, browserHistory } from 'react-router'
+
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import AgeCheck from './components/AgeCheckPage'
 import Home from './containers/Home'
 import Forms from './containers/Forms'
 import OverAge from './components/OverAge'
+import NotFound from './components/NotFound'
 import { Provider } from 'react-redux'
 import { createStore, combineReducers } from 'redux'
 import yoti from './reducers/yoti'
@@ -14,20 +18,22 @@ import muiTheme from './assets/theme'
 
 const reducers = combineReducers({
   yoti,
-  forms
+  forms,
+  routing: routerReducer
 })
 
 const store = createStore(reducers)
+const history = syncHistoryWithStore(browserHistory, store)
 
 ReactDOM.render(
   <MuiThemeProvider muiTheme={muiTheme}>
     <Provider store={store}>
-      <Router history={browserHistory}>
+      <Router history={history}>
         <Route path='/' component={Home} />
         <Route path='/age-check' component={AgeCheck} />
         <Route path='/form' component={Forms} />
         <Route path='/over-age' component={OverAge} />
-        <Route path='*' component={Home} />
+        <Route path='/*' component={NotFound} />
       </Router>
     </Provider>
   </MuiThemeProvider>,
