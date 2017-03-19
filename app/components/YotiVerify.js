c5ee2a2569b4567a1a574a7b4620188c889aedcd
WilliamSavage <willsavage@hotmail.com>
2017-03-19 12:58:36 +0200

    adds route for asset files and add corresponign path to requests

--- app/components/YotiVerify.js
+++ app/components/YotiVerify.js
@@ -6,7 +6,7 @@ const YotiVerify = () => {
     <div className='yoti-description-container'>
       <h3 className='yoti-description-title'>VERIFY YOUR ARE USING YOTI</h3>
       <span className='yoti-description'>
-        <img src='./imgs/yoti-logo.svg' alt='yoti logo' />
+        <img src='/assets/imgs/yoti-logo.svg' alt='yoti logo' />
         <p>If you've already got a YOTI account, click the button below and scan our QR code using your YOTI.</p>
       </span>
       <p className='yoti-instructions'>
