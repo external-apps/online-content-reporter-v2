617c56caedf4f52b773c016d70f57ade49fe2d67
WilliamSavage <willsavage@hotmail.com>
2017-03-20 15:24:15 +0200

    #90 combines routerReducer and applies saga middleware

--- app/index.js
+++ app/index.js
@@ -1,42 +1,49 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import { Route, Router, browserHistory } from 'react-router'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import { syncHistoryWithStore, routerMiddleware, routerReducer } from 'react-router-redux'
+import yotiSaga from './actions/yoti'
+import yoti from './reducers/yoti'
+import forms from './reducers/forms'
+// Styling & Themes
+import muiTheme from './assets/theme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+// Components
 import AgeCheck from './components/AgeCheckPage'
 import Home from './containers/Home'
 import Forms from './containers/Forms'
 import OverAge from './components/OverAge'
 import NotFound from './components/NotFound'
-import { Provider } from 'react-redux'
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import yoti from './reducers/yoti'
-import forms from './reducers/forms'
-import muiTheme from './assets/theme'
-
-import createSagaMiddleware from 'redux-saga'
-import mySaga from './actions/yoti'
 
 // create the saga middleware
 const sagaMiddleware = createSagaMiddleware()
 
+const middleware = [ sagaMiddleware, routerMiddleware(browserHistory) ]
+
 const reducers = combineReducers({
   yoti,
-  forms
+  forms,
+  routing: routerReducer
 })
 
 // mount saga on the Store
 const store = createStore(
   reducers,
-  applyMiddleware(sagaMiddleware)
+  applyMiddleware(...middleware)
 )
 
 // then run the saga
-sagaMiddleware.run(mySaga)
+sagaMiddleware.run(yotiSaga)
+
+const history = syncHistoryWithStore(browserHistory, store)
 
 ReactDOM.render(
   <MuiThemeProvider muiTheme={muiTheme}>
     <Provider store={store}>
-      <Router history={browserHistory}>
+      <Router history={history}>
         <Route path='/' component={Home} />
         <Route path='/age-check' component={AgeCheck} />
         <Route path='/form' component={Forms} />
