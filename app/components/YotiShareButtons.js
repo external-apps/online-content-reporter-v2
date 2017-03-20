c6c911539e05217f155ec11181b9b620c6746209
WilliamSavage <willsavage@hotmail.com>
2017-03-20 15:23:40 +0200

    #90 moves all controller/middleware async actions into yoti action and intoa  saga

--- app/components/YotiShareButtons.js
+++ app/components/YotiShareButtons.js
@@ -1,78 +1,16 @@
 import React from 'react'
 import '../scss/style.scss'
 import RaisedButton from 'material-ui/RaisedButton'
-import { browserHistory } from 'react-router'
-import axios from 'axios'
 
 class YotiShareButtons extends React.Component {
-  constructor (props) {
-    super(props)
-    this.getQr = this.getQr.bind(this)
-    // this.listenForToken = this.listenForToken.bind(this)
-    this.yotiRedirect = this.yotiRedirect.bind(this)
-    // this.verifyAgeMobile = this.verifyAgeMobile.bind(this)
-  }
-
   componentWillMount () {
     if (!this.props.yoti.isMobile) {
-      this.getQr()
+      this.props.qrFetchRequested()
       this.props.closeQr()
     }
   }
 
-  getQr () {
-    this.props.qrFetchRequested()
-    // axios.get('/get-qr')
-    // .then(res => {
-    //   this.props.addQr(res.data.svg)
-    //   this.listenForToken(res.data.proto, res.data.url)
-    // })
-    // .catch((error) => {
-    //   console.log(error)
-    // })
-  }
-  //
-  // listenForToken (proto, url) {
-  //   var host = 'wss://api.yoti.com/api/v1/connect-sessions/' + proto
-  //   var socket = new WebSocket(host)
-  //   socket.onopen = () => {
-  //     socket.send(JSON.stringify({subscription: proto}))
-  //   }
-  //   socket.onmessage = (msg) => {
-  //     this.props.ageIsVerified()
-  //     var data = JSON.parse(msg.data)
-  //     switch (data.status) {
-  //       case 'COMPLETED' : {
-  //         this.yotiRedirect(data.token)
-  //       }
-  //     }
-  //   }
-  // }
-
-  yotiRedirect (token) {
-    axios.get(`/thankyou?token=${token}`)
-    .then(res => {
-      if (res.data.isUnder18) {
-        browserHistory.push('/form')
-      } else browserHistory.push('/over-age')
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-  }
-
-  // verifyAgeMobile (href) {
-  //   axios.get(href)
-  //   .then(res => {
-  //     res.data.isUnder18 ? browserHistory.push('/form') : browserHistory.push('/over-age')
-  //   })
-  //   .catch((error) => {
-  //     console.log(error)
-  //   })
-  // }
-
   render () {
-    console.log('YOTI COMPONENT PROPS: ', this.props);
     const clickHandler = this.props.yoti.isMobile ? null : (this.props.openQr)
     return (
       <div>
