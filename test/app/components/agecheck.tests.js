59d81bcb5f6ed51d57d2ea3e6e912ef367b0b73d
denesnori <denes.nori@yahoo.com>
2017-03-20 14:27:45 +0000

    fix tests

--- test/app/components/agecheck.tests.js
+++ test/app/components/agecheck.tests.js
@@ -1,6 +1,6 @@
 import { expect } from 'chai'
 import React from 'react'
-import { shallow, mount, render } from 'enzyme'
+import { shallow } from 'enzyme'
 import AgeCheck from '../../../app/components/AgeCheckPage'
 
 describe('<AgeCheck />', () => {
