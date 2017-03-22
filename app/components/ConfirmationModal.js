dac4e6fdcf79d054cf7e56a5ec1f83f11d34d3b9
denesnori <denes.nori@yahoo.com>
2017-03-22 14:59:02 +0000

    chasing bugs

--- app/components/ConfirmationModal.js
+++ app/components/ConfirmationModal.js
@@ -50,7 +50,7 @@ class ConfirmationModal extends React.Component {
           <RaisedButton
             className='close_btn'
             primary={true} label='X'
-            onClick={() => this.props.closeModal()} />
+            onClick={() => {console.log('I am being called!!');console.log(this.props);this.props.closeModal()}} />
           <h2 className='red'>Thank you.</h2>
           <h2>We have sent your report to the Internet Watch Foundation (IWF) who will review your request.</h2>
           <p>
