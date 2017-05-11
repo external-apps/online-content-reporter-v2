350ec26a4343e08257da38c792d01e5d70216472
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2017-05-11 11:49:19 +0100

    move qr code render condition check to top level
    related #5

--- app/components/QrCode.js
+++ app/components/QrCode.js
@@ -2,7 +2,9 @@ import React from 'react'
 import '../scss/style.scss'
 
 const QrCode = (props) => (
-  <span dangerouslySetInnerHTML={{__html: props.yoti.qrSvg}} />
+  <div className='yoti-qr'>
+    <span dangerouslySetInnerHTML={{__html: props.yoti.qrSvg}} />
+  </div>
 )
 
 export default QrCode
--- app/containers/Yoti.js
+++ app/containers/Yoti.js
@@ -1,29 +1,32 @@
 import React from 'react'
 import { connect } from 'react-redux'
+
+import QrCode from '../components/QrCode'
 import * as yotiActions from '../actions/yoti'
 import YotiShareButtons from '../components/YotiShareButtons'
 import YotiDescription from '../components/YotiDescription'
 import '../scss/style.scss'
 
 class Yoti extends React.Component {
-  renderScanMe () {
-    if (this.props.yoti.showQr) {
-      return (
-        <p className='scan-me'>
-          SCAN ME WITH YOTI
-          <img src='assets/imgs/yoti-logo.svg' className='popup-yoti' alt='yoti logo' />
-        </p>
-      )
-    }
-  }
-
   render () {
     return (
-      <div className='yoti-info'>
-        <YotiDescription {...this.props} />
-        <YotiShareButtons {...this.props} />
-        {this.renderScanMe()}
-      </div>
+      <div className='yoti-info'> {
+        this.props.yoti.showQr ? (
+          <div>
+            <QrCode {...this.props} />
+            <YotiShareButtons {...this.props} />
+            <p className='scan-me'>
+              SCAN ME WITH YOTI
+              <img src='assets/imgs/yoti-logo.svg' className='popup-yoti' alt='yoti logo' />
+            </p>
+          </div>
+        ) : (
+          <div>
+            <YotiDescription />
+            <YotiShareButtons {...this.props} />
+          </div>
+        )
+      } </div>
     )
   }
 }
