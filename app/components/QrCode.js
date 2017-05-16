052f1b94422c4c8d69619b8a5c9d811eb0b9af56
WilliamSavage <willsavage@hotmail.com>
2017-05-16 20:17:04 +0100

    #28 adds back button to qr code

--- app/components/QrCode.js
+++ app/components/QrCode.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import '../scss/style.scss'
+import ArrowBack from 'material-ui/svg-icons/navigation/arrow-back'
 
 const QrCode = (props) => (
   <div>
+    <span className='scan-back-button' onClick={() => props.closeQr()}>
+      <ArrowBack />
+      Back
+    </span>
     <div className='yoti-qr'>
       <img src={props.yoti.qrSvg} />
     </div>
